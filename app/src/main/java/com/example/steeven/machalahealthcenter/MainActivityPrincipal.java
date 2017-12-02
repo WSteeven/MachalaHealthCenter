81293eca8122c6cd718f3ae76f703edee61d452f
WSteeven <34004641+WSteeven@users.noreply.github.com>
2017-12-02 13:45:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/steeven/machalahealthcenter/MainActivityPrincipal.java
@@ -0,0 +1,13 @@
+package com.example.steeven.machalahealthcenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_principal);
+    }
+}
